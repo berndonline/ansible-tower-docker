075ec69603dfe9dc25a73c27558e2dac778d0a69
Ubuntu <ubuntu@de-srv-01.hostgate.net>
2018-11-21 20:11:41 +0000

    changed entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,12 +6,6 @@ trap "kill -15 -1 && echo all proc killed" TERM KILL INT
 
 if [ "$1" = "ansible-tower" ]; then
 
-	if [[ -a /certs/license ]]; then
-		echo "copy new license"
-		cp -r /certs/license /etc/tower/license
-		chown awx:awx /etc/tower/license
-        fi
-	
 	ansible-tower-service start
 	sleep inf & wait
 else
